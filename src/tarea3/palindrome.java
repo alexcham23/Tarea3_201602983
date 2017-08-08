4b8e49289ad411ccce898daa49cae123848c2288
byron <byron@192.168.1.4>
2017-08-07 22:26:00 -0600

    fin comit

--- src/tarea3/palindrome.java
+++ src/tarea3/palindrome.java
@@ -12,18 +12,28 @@ package tarea3;
 import java.util.Scanner;
 public class palindrome {
     public void palindrome () {
-    Scanner nose =new Scanner(System.in);
-    String str = nose.nextLine();
-    int cont = 0;
-    for(int i=0; i < str.length()-1; i++) {
-if(str.charAt(i) !=str.charAt(str.length()-1-i)){
-        break;
-}
-if(cont==0)
-  System.out.println("loa");
-else
-   System.out.println("perro");
-}
-    }
+    Scanner teclado = new Scanner(System.in); 
+        String nombre, nombre1 = "",nombre2 = ""; 
+        System.out.println("Ingrese un nombre"); 
+        nombre = teclado.nextLine();
+        for (int i = 0; i < nombre.length(); i++) { 
+        	if(nombre.codePointAt(i)>96 && nombre.codePointAt(i)<123) {
+	            nombre1 = nombre1 + nombre.charAt(i);
+        	}
+        }
+        
+        for (int i = nombre.length()-1; i >= 0; i--) { 
+        	if(nombre.codePointAt(i)>96 && nombre.codePointAt(i)<123) {
+	            nombre2 = nombre2 + nombre.charAt(i);
+        	}
+        }
+                
+        if (nombre1.equals(nombre2)) { 
+            System.out.println("La palabra es palindroma");  
+        }else{
+            System.out.println("No es palindroma");
+        }
+ 
+	}
    }
 
