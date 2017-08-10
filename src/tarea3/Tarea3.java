34a1808822a45321cc0c6cb4e087a018f43165d4
byron <byron@192.168.1.4>
2017-08-09 18:34:05 -0600

    "Tarea 3 finalizada con Éxito"

--- src/tarea3/Tarea3.java
+++ src/tarea3/Tarea3.java
@@ -5,7 +5,12 @@ public class Tarea3 {
 
     
     public static void main(String[] args) {
-     menu llamada = new menu();  
+    System.out.println("---------------");
+    System.out.println("    Tarea 3     ");
+    System.out.println("   201602983     ");
+    System.out.println("---------------");
+    
+        menu llamada = new menu();  
      llamada.metodo1();
     }
     
